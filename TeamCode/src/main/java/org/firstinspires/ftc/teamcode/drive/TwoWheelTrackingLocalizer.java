edc4a081c81d732ae2d82ce2b8dae9404f7ff2b1
SomeGuyWithGithub <129338880+SomeGuyWithGithub@users.noreply.github.com>
2024-02-11 22:22:37 -0800

    this not working, not very fun

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -36,9 +36,10 @@ import java.util.List;
  */
 @Config
 public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
-    public static double TICKS_PER_REV = 384.5;
-    public static double WHEEL_RADIUS = 1.8898; // in
+    public static double TICKS_PER_REV = 2000;
+    public static double WHEEL_RADIUS = 0.944882; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
+
 
     public static double PARALLEL_X = -1; // X is the up and down direction
     public static double PARALLEL_Y = 0; // Y is the strafe direction
@@ -65,9 +66,7 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
         this.drive = drive;
 
         parallelEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRight"));
-        perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "odo1"));
-
-        // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
+        perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backRight"));
     }
 
     public static double encoderTicksToInches(double ticks) {
