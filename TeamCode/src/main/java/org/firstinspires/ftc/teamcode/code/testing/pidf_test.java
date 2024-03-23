a1e31ada6c91ca5979425c7e57f052b0bb90944c
SomeGuyWithGithub <129338880+SomeGuyWithGithub@users.noreply.github.com>
2024-03-22 18:24:35 -0700

    this sucks ass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/testing/pidf_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/testing/pidf_test.java
@@ -3,40 +3,44 @@ package org.firstinspires.ftc.teamcode.code.testing;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.code.baseClasses.basePIDF;
 import org.firstinspires.ftc.teamcode.code.constants.Consts;
-import org.firstinspires.ftc.teamcode.code.constants.PIDFConsts;
 
 @Config
 @TeleOp
 public class pidf_test extends OpMode {
     Consts consts;
-    PIDFConsts.slidePIDF slidePIDF;
+
+    PIDController controller;
 
     public static double p = 0, i = 0, d = 0, f = 0;
+    public final double ticksPerDegree = 537.7 / 360;
 
     public static int target = 0;
+
+    public double getCurrentPos() {
+        return (consts.slideL.getCurrentPosition() + consts.slideR.getCurrentPosition()) / 2.;
+    }
 
     @Override
     public void init() {
         consts = new Consts(hardwareMap);
-        slidePIDF = new PIDFConsts.slidePIDF(hardwareMap);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void loop() {
-        basePIDF.p = p;
-        basePIDF.i = i;
-        basePIDF.d = d;
-        basePIDF.f = f;
+        controller.setPID(p, i, d);
 
-        slidePIDF.setMotors(target);
+        double pid = controller.calculate(getCurrentPos(), target);
+        double ff = Math.cos(Math.toRadians(target / ticksPerDegree)) * f;
 
-        telemetry.addData("position ", slidePIDF.getCurrentPos());
-        telemetry.addData("target ", target);
+        double power = pid + ff;
+
+        consts.slideR.setPower(power);
+        consts.slideL.setPower(power);
     }
 }
